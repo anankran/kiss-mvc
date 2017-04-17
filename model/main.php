c55bd40c9129b199f99f242587b42c0e5cdc694c
André Nankran <nankran@inventati.org>
2017-04-17 15:31:26 -0300

    Update main.php

--- model/main.php
+++ model/main.php
@@ -9,32 +9,32 @@ namespace Model;
 
 class Main extends Database {
 
-	/**
-   * Search all results
-   * @uses Database
-   * @access public
-   * @return obj
-  */
+  	/**
+   	* Search all results
+   	* @uses Database
+   	* @access public
+   	* @return obj
+  	*/
 	public static function all()
 	{
 		$sql = 'SELECT * FROM main';
-    $records = Database::select($sql);
-    return $records;
+    		$records = Database::select($sql);
+    		return $records;
 	}
 
 	/**
-   * Filtered search
-   * @param $state integer Unique ID
-   * @uses Database
-   * @access public
-   * @return obj
-  */
+   	* Filtered search
+   	* @param $state integer Unique ID
+   	* @uses Database
+   	* @access public
+   	* @return obj
+ 	 */
 	public static function filter($id)
 	{
 		$sql = 'SELECT * FROM main WHERE field = ?';
 		Database::setFields(array($id));
-    $records = Database::select($sql);
-    return $records;
+   		$records = Database::select($sql);
+    		return $records;
 	}
 
 }
